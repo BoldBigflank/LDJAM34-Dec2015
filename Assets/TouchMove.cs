e1ef3b4e055e4ddd0d71eea70775fc04bba6e9c6
Alex Swan <alex@bold-it.com>
2015-12-11 21:00:31 -0800

    initial

---
+++ Assets/TouchMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class TouchMove : MonoBehaviour {
+
+	public float speed = 6.0F;
+    public Camera forwardCamera;
+
+    void Update() {
+        CharacterController controller = GetComponent<CharacterController>();
+        Vector3 forward = forwardCamera.transform.forward;
+        float curSpeed = (Input.touchCount > 0 || Input.GetMouseButton(0)) ? speed : 0.0f;
+        controller.SimpleMove(forward * curSpeed);
+    }
+}
