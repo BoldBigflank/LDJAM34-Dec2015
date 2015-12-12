5f76f2099ed949da91cfbc5b56d4c92aa9d0dc40
Alex Swan <alex@bold-it.com>
2015-12-12 01:23:04 -0800

    Use layers to show only what's pertinent in Remote screen

---
+++ Assets/RemoteVisibility.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoteVisibility : MonoBehaviour {
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( gameObject.layer != LayerMask.NameToLayer("Below") && 
+			transform.position.y < player.transform.position.y){
+			SetLayer(LayerMask.NameToLayer("Below"));
+
+		} else if ( gameObject.layer != LayerMask.NameToLayer("Above") && 
+			transform.position.y > player.transform.position.y ){
+			SetLayer(LayerMask.NameToLayer("Above"));
+		}
+	}
+
+	void SetLayer(int layer){
+		gameObject.layer = layer;
+		foreach(Transform child in transform){
+			child.gameObject.layer = layer;
+		}
+	}
+}
